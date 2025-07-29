361a9a96761e353f443c21691474fd9b54b3d9de
Rudolf Kopotin <kopkopkop9120@gmail.com>
2025-07-29 19:37:56 +0300

    Initial commit

---
+++ order-service/src/test/java/org/turbostore/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.turbostore.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ payment-service/src/test/java/org/turbostore/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.turbostore.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
